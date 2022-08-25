b9812f59ff7f3ea55a142f385c6302f1e0c761d4
Randy West <randywest55@gmail.com>
2022-08-25 15:23:33 -0700

    Add basic app skeleton w/theme & github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,53 @@
+import {
+  CssBaseline,
+  IconButton,
+  Paper,
+  ThemeProvider,
+  Tooltip,
+  unstable_createMuiStrictModeTheme,
+} from "@mui/material";
+import { blueGrey } from "@mui/material/colors";
+import { GitHub } from "@mui/icons-material";
+
 function App() {
-  return <></>;
+  const theme = unstable_createMuiStrictModeTheme({
+    palette: { mode: "dark", primary: blueGrey },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div
+        id="app"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Paper
+          elevation={5}
+          // TODO: make this responsive
+          style={{ width: 500, height: 500, margin: 20 }}
+        ></Paper>
+        <Tooltip
+          title="See the code on github.com"
+          placement="top"
+          arrow
+          disableInteractive
+        >
+          <IconButton
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/thisisrandy/multi-chess-clock"
+          >
+            <GitHub />
+          </IconButton>
+        </Tooltip>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
